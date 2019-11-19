26538ccd1723e8e700ee8c3e5d57e6acbe95182f
SANYAM <sanyamsingh.96@gmail.com>
2019-11-19 12:02:07 +0530

    removing punctuations feature added

--- oursite/oursite/views.py
+++ oursite/oursite/views.py
@@ -5,16 +5,28 @@ from django.shortcuts import render
 def index(request):
     
     return render(request,'index.html')
-def about(request):
-    return HttpResponse('this website is created by sanyam and this is his first website in the platform of the django')
-def website(request):
-    return HttpResponse('''<h1>GO TO WEBSITE</h1<a href ="https://www.youtube.com/results?search_query=call+of+duty+modern+warfare+playlist+"> Django code</a>''')
-def removepunc(request):
+# def about(request):
+#     return HttpResponse('this website is created by sanyam and this is his first website in the platform of the django')
+# def website(request):
+#     return HttpResponse('''<h1>GO TO WEBSITE</h1<a href ="https://www.youtube.com/results?search_query=call+of+duty+modern+warfare+playlist+"> Django code</a>''')
+def analyze(request):
     # get the text
     djtext = request.GET.get('text','default')
+    removepunc = request.GET.get('removepunc','off')
+    print(removepunc)
     print(djtext)
+    # analyzed = djtext
     # analyze the text
-    return HttpResponse("remove punc")
+    if removepunc == 'on':
+        Punctuations = '''!()-[]{};:'"\,<>.?@#$%^&*+_~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in Punctuations:
+                analyzed = analyzed + char
+        params = {'purpose':'Removed Punctuations','analyzed_text':analyzed}
+        return render('request','analyze.html',params)
+    else:
+        return HttpResponse('Error')
 # def capfirst(request):
 #     return HttpResponse("capitalize first")
 # def newlineremove(request):
@@ -23,5 +35,3 @@ def removepunc(request):
 #     return HttpResponse("spaceremove")
 # def charcount(request):
 #     return HttpResponse("charcount")
-def analyze(request):
-    return HttpResponse('analyze')
